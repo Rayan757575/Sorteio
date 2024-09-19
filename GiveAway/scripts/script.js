75e2df8e6b294249d8b2e280bd8b700e034263a9
Rayan <141056772+Rayan757575@users.noreply.github.com>
2024-09-19 07:57:57 -0300

    Delete GiveAway/scripts directory

--- GiveAway/scripts/script.js
+++
@@ -1,8 +0,0 @@
-//recebe objetos da página atual
-const campo = document.getElementById('campo');
-
-//da o foco para o input ao abrir ou relogar a página
-window.addEventListener('load', () => {
-    campo.focus();
-});
-
