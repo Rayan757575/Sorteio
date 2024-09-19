9cc488a72c3b2e285512a41361b7e3251f1b2396
Rayan <141056772+Rayan757575@users.noreply.github.com>
2024-09-19 07:57:46 -0300

    Delete GiveAway/backEnd directory

--- GiveAway/backEnd/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $dbHost = 'localhost';
-    $dbUserName = 'root';
-    $dbPassoword = '';
-    $dbName = 'escola';
-    
-    $conexao = new mysqli($dbHost, $dbUserName, $dbPassoword , $dbName);
-   
-?>
