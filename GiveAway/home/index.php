d6566c3118eb5d54bee1e84dbae1135688f34b50
Rayan <141056772+Rayan757575@users.noreply.github.com>
2024-09-19 08:23:15 -0300

    Add files via upload

--- GiveAway/home/index.php
+++ GiveAway/home/index.php
@@ -1,5 +1,5 @@
 <?php
-    include_once('./view/header.php'); // all page's configurantion
-    include_once('./view/input.php'); // the form 
-    include_once('./view/table.php'); // the table that shows the pepople who have already registered
+    include_once('../view/header.php'); // all page's configurantion
+    include_once('../view/input.php'); // the form 
+    include_once('../view/table.php'); // the table that shows the pepople who have already registered
 ?>
